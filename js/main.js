a0017dd365e16afc3a1c3a4d3675c0ce86c723c1
Jzhen123 <jiayuzheng01@gmail.com>
2021-03-11 10:04:13 -0500

    Established MVC structure

--- js/main.js
+++ js/main.js
@@ -1,19 +1,57 @@
-var Model = {
-
+class Model {
+    constructor(){
+        this.c = null;
+    }
+    setController(controller){
+        this.c = controller;
+    }
 }
 
-var View = {
-
+class View {
+    constructor(){
+        this.m = null;
+    }
+    setModel(model){
+        this.m = model;
+    }
+    // constructor() {
+    //     this.x = "0px";
+    //     this.y ="0px";
+    // } 
+    // createGrid (gridX, gridY) {
+    //     let exampleDiv = document.createElement('div');
+    //     exampleDiv.style.width = gridX;
+    //     exampleDiv.style.height = gridY;
+    //     this.x = gridX;
+    //     this.y = gridY;
+    // }
 }
 
-var Controller = {
+class Controller {
+    constructor(model, view){
+        this.m = model;
+        this.v = view;
 
+    }
 }
 
-var App = {
-    
+class App {
+    constructor() {
+        this.m = new Model();
+        this.v = new View();
+        this.v.setModel(this.m);
+        this.c = new Controller(this.m, this.v);
+        this.m.setController(this.c);
+    }
+    init(){
+        console.log("App is starting")
+        this.c.init();
+    }
 }
+
 
 function init() {
-    
+    let a = new App();
+    a.init();
 }
+
