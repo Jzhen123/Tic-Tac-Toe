efc0751adcb9c68dec63ef60f4475233f2510dc4
Jzhen123 <jiayuzheng01@gmail.com>
2021-03-12 10:24:23 -0500

    Demo Day Commit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ class Model {
     constructor() {
         this.c = null;
         this.counter = 0;
+        this.array = [];
     }
+    // Initializes Grid array of 0s and Displays a string based on counter = 0
     init() {
         this.createGridArray();
         this.whoseTurn();
@@ -10,45 +12,48 @@ class Model {
     setController(controller) {
         this.c = controller;
     }
-    setState(s){
+    setState(s) {
         this.counter = s;
         this.onSetState();
+        
     }
-    getState(){
+    getState() {
         this.whoseTurn();
         return this.turn;
     }
-    onSetState(){
+    onSetState() {
         this.c.updateView();
     }
-    createGridArray(){
-        this.array = [];
+    // Creates an array of 0s to be manipulated later by event listeners
+    createGridArray() {
         for (let i = 0; i < 9; i++) {
             // console.log(this.array)
             this.array.push(0);
         }
     }
-    whoseTurn(){
+    // Changes whos the text saying whos turn it is based off of the counter clicks
+    whoseTurn() {
         let playerOne = "Player 1's Turn";
         let playerTwo = "Player 2's Turn";
-        if (this.counter == 0){
-            this.turn = "Hi"
-        } else if (this.counter > 8){
+        if (this.counter == 0) {
+            this.turn = playerOne
+        } else if (this.counter > 8) {
             this.turn = "Game over stand in"
         } else if (this.counter % 2 != 0) {
             this.turn = playerOne
         } else if (this.counter % 2 == 0) {
             this.turn = playerTwo
-        } 
+        }
     }
 
 }
 
 class View {
-    constructor() { 
+    constructor() {
         this.m = null;
     }
-    init(){
+    // Initiates the Page with headers and the board
+    init() {
         this.createView();
     }
 
@@ -70,23 +75,23 @@ class View {
     // Multiple generateHtml functions to create the main view
     createView() {
         this.app = document.getElementById('app');
-        this.header = this.generateHtml("h2", ["text-center"], app, "Hello")
-        this.counter_txt = this.generateHtml("h3", ["text-center"], app, this.m.turn)
+        this.header = this.generateHtml("h2", ["text-center", "mt-4"], app, "Welcome to Tic Tac Toe!")
+        this.counter_txt = this.generateHtml("h3", ["text-center", "pt-2"], app, this.m.turn)
         this.container = this.generateHtml("div", ["container", "p-5"], app)
         this.board = this.generateHtml("div", ["row"], this.container)
         for (let i = 0; i < 9; i++) {
             let col = this.generateHtml("div", ["col-4", "border", "border-2"], this.board, i, this.m.c.incrementCounter.bind(this));
-            col.setAttribute("id", "col" + i)
+            col.setAttribute("id", i)
         }
     }
-    render(){
+    render() {
         this.counter_txt.textContent = this.getState();
     }
-    getState(){
+    getState() {
         var c = this.m.getState();
         return c;
     }
-    
+
 }
 
 class Controller {
@@ -94,20 +99,30 @@ class Controller {
         this.m = model;
         this.v = view;
     }
-    init() {   
+    init() {
     }
-// Function that can be used to be added to an event listener to increment the counter
-    listener(num) {
-        this.incrementCounter();
-    }
-// Function that adds one to the counter variable in the Model
-    incrementCounter(){
-        this.m.setState(this.m.counter+1)
-        console.log(this.m.counter)
-    }
-    updateView(){
+
+    updateView() {
         this.v.render();
     }
+    // Function that can be used to be added to an event listener function
+    listener() {
+        this.incrementCounter();
+        this.changeArray();
+    }
+    // Trying to manipulate the Grid Array based on which col is clicked
+    changeArray(){
+        if (this.m.turn == this.m.playerOne){
+            this.m.array[this.v.col.id] = 1
+        }
+    }
+    // Function that adds one to the counter variable in the Model
+    incrementCounter() {
+        this.m.setState(this.m.counter + 1)
+        console.log(this.m.counter)
+        console.log(this.m.array)
+    }
+
 }
 
 class App {
@@ -126,7 +141,6 @@ class App {
         this.v.init();
     }
 }
-
 
 function init() {
     let a = new App();
